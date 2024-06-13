26aedcda3abb0cae691794b2dda478a4c93077f6
Willard Owiti Odongo <willard.odongo10@gmail.com>
2024-06-13 20:08:33 +0300

    more changes on user interface

--- user_interface.py
+++ user_interface.py
@@ -1,66 +1,33 @@
 import streamlit as st
 import retrieval_augmented_generation as rag
+import time
 
 def load_questions(file_path):
     with open(file_path, 'r') as file:
         questions = file.readlines()
     return [question.strip() for question in questions]
 
+def display_typing_effect(bot_message, delay=0.05):
+    bot_message_container = st.empty()
+    bot_message_display = ""
+    for char in bot_message:
+        bot_message_display += char
+        bot_message_container.markdown(f'<div class="stMarkdown bot-message"><b>Maziwa Max</b> <br> {bot_message_display}</div>', unsafe_allow_html=True)
+        time.sleep(delay)
+    return bot_message_container
+
 def main():
     st.set_page_config(page_title="MaziwaMax", page_icon=":cow:")
 
-    # CSS styling for messages
-    st.markdown("""
-        <style>
-        .header{
-            text-align: center;
-        }
-        .user-message {
-            background-color: #999999;
-            border-radius: 5px;
-            padding: 10px;
-            margin-top: 10px;
-            color: black;
-            float: right;
-            display: inline-block;
-        }
-        .bot-message {
-            border-radius: 5px;
-            padding: 10px;
-            margin-bottom: 10px;
-            text-align: left;
-            display: inline-block;
-        }
-        .input-box {
-            position: fixed;
-            bottom: 0;
-            width: 70%;
-            padding: 10px;
-            display: flex;
-        }
-        .input-box input {
-            width: 50%;
-            padding: 10px;
-            border-radius: 5px;
-        }
-        </style>
-        """, unsafe_allow_html=True)
+    # Load CSS from file
+    with open('./style/styles.css') as f:
+        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
     st.markdown("<h1 class='header'>MaziwaMax</h1>", unsafe_allow_html=True)
 
     # Initialize the conversation history if not already done
     if 'conversation_history' not in st.session_state:
         st.session_state.conversation_history = []
-
-    # Display the conversation history
-    for turn in st.session_state.conversation_history:
-        if "user" in turn:
-            st.markdown(f'<div class="stMarkdown user-message"><b>Farmer</b> <br> {turn["user"]}</div>', unsafe_allow_html=True)
-        else:
-            st.markdown(f'<div class="stMarkdown bot-message"><b>Maziwa Max</b> <br> {turn["MaziwaMax"]}</div>', unsafe_allow_html=True)
-
-    # Placeholder for the spinner
-    spinner_placeholder = st.empty()
 
     # Suggested questions for first-time users
     suggested_questions = load_questions('./data/questions.txt')
@@ -74,6 +41,16 @@ def main():
             if st.button(question):
                 st.session_state.user_query = question
                 st.session_state.first_visit = False
+
+    # Display the conversation history
+    for turn in st.session_state.conversation_history:
+        if "user" in turn:
+            st.markdown(f'<div class="stMarkdown user-message"><b>Farmer</b> <br> {turn["user"]}</div>', unsafe_allow_html=True)
+        else:
+            st.markdown(f'<div class="stMarkdown bot-message"><b>Maziwa Max</b> <br> {turn["MaziwaMax"]}</div>', unsafe_allow_html=True)
+
+    # Placeholder for the spinner
+    spinner_placeholder = st.empty()
 
     # Text input box at the bottom
     with st.form(key='user_query_form', clear_on_submit=True):
@@ -94,6 +71,9 @@ def main():
 
             # Clear the spinner
             spinner_placeholder.empty()
+
+            # Refresh the display to show the latest user query
+            st.experimental_rerun()
 
 if __name__ == '__main__':
     main()
